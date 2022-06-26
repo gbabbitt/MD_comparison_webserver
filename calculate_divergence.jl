04a6194b242e4e438852ba6b74bad86e0bc0530d
cjryanwashere <91854094+cjryanwashere@users.noreply.github.com>
2022-06-26 12:32:37 -0400

    Add files via upload

--- calculate_divergence.jl
+++ calculate_divergence.jl
@@ -1,3 +1,10 @@
-#load .npy files to run calculations on
+#I know this is incredibly simple, it will be updated
 using Pkg
-Pkg.add("Npy")
+Pkg.add("Distances")
+using Distances
+
+ref = [1.3,1.2,1.1]
+que = [1.5,1.2,1.6]
+
+result = js_divergence( ref, que)
+print(result)
